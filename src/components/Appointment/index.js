b477fc0aebb0c4e584a7048cafda6ea3635726b7
monica-li205 <monicali@live.ca>
2020-12-10 16:51:28 -0800

    adds functionality to the Empty and Cancel buttons

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,39 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form"
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment (props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
   <article className="appointment" >
     <Header time={props.time}/>
-    {props.interview ? (
-        <Show
-        id={props.id}
-        time={props.time}
-        interview={props.interview}
-        interviewer={props.interview["interviewer"]["name"]}
-        />
-    ) : (
-        <Empty/>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+    <Show
+      id={props.id}
+      time={props.time}
+      interview={props.interview}
+      interviewer={props.interview["interviewer"]["name"]}
+    />
     )}
+    {mode === CREATE && 
+    <Form 
+      interviewers={[]}
+      interviewer={null}
+      onSave={props.onSave}
+      onCancel={() => back(EMPTY)}
+    />}
   </article>
   )
 }
+
